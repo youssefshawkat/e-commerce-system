1923ff30ac3cf3f2349ec5a6ed89c1751fefa4c3
youssefshawkat <youssefshawkat26@gmail.com>
2025-07-05 01:04:22 +0300

    created the product class

---
+++ Product.java
@@ -0,0 +1,35 @@
+public abstract class Product {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
